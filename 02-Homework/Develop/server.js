0987173df8b916b7f55f13396106e543b8e508dc
ahmadmujeeb33 <ahmadmujeeb333@gmail.com>
2021-06-17 22:42:15 -0400

    server created and links working

--- 02-Homework/Develop/server.js
+++ 02-Homework/Develop/server.js
@@ -21,7 +21,7 @@ app.get('/notes',(req,res) =>{
     res.sendFile(path.join(__dirname,'./public/notes.html'));
 })
 
-app.post('/api/notes',(req,res) =>{
+app.post('/notes',(req,res) =>{
     console.log("in here");
     console.log(req.body);
 })
