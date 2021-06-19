377b37b07e89dbf1fe0c8982e671ee71400abde2
ahmadmujeeb33 <ahmadmujeeb333@gmail.com>
2021-06-18 20:29:34 -0400

    still working on it

--- 02-Homework/Develop/server.js
+++ 02-Homework/Develop/server.js
@@ -9,6 +9,8 @@ const app = express();
 
 const PORT = 3000;
 
+let newData ;
+
 app.use(express.json());
 app.use(express.urlencoded({extended: false}))
 
@@ -26,34 +28,34 @@ app.get('/notes',(req,res) =>{
 app.get('/api/notes',(req,res) =>{
     console.log("thisss");
     fs.readFile('./db/db.json','utf8', (err,data) =>{
+        newData = JSON.parse(data)
         return res.json(JSON.parse(data));
+        
     });
 })
 
 app.post('/api/notes',(req,res) =>{
     console.log("in post");
     let values = req.body;
-    fs.readFile('./db/db.json','utf8', (err,data) =>{
-        console.log(typeof data);
-        console.log("in readfile");
-        newData = JSON.parse(data);
-        console.log(typeof newData);
-        console.log("data pushed");
-        console.log(newData);
+    // fs.readFile('./db/db.json','utf8', (err,data) =>{
+        // console.log(typeof data);
+        // console.log("in readfile");
+        // newData = JSON.parse(data);
+        // console.log(typeof newData);
+        // console.log("data pushed");
+        // console.log(newData);
 
-        newData.push(values);
-        console.log("rthusdhoxc");
-        console.log(newData);
+     newData.push(values);
+        // console.log("rthusdhoxc");
+        // console.log(newData);
         fs.writeFile("./db/db.json",JSON.stringify(newData),(err) =>{
-            console.log("in this one")
-            console.log(newData);
-
-
-            return res.json(newData)
+            // console.log("in this one")
+            // console.log(newData);
+             res.json(newData)
         })
               
 
-    });
+    // });
 
     // console.log(newInfo);
 
